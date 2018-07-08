56e91d9962f42fabc5e382c3a28dd06193b77b2b
nateshmbhat <nateshmbhat1@gmail.com>
2018-07-08 17:20:32 +0530

    added forces to environment

--- src/assets/js/sketch.js
+++ src/assets/js/sketch.js
@@ -4,123 +4,142 @@
 let totalwidth = 1000,
     totalheight = 800;
 let mouseDown = 0;
-let canvasWidth = totalwidth ; 
-let canvasHeight= totalheight; 
+let canvasWidth = totalwidth;
+let canvasHeight = totalheight;
 // let canvasWidth  = document.body.clientWidth ; 
 // let canvasHeight = document.body.clientHeight ; 
-let environ  ; 
+let environ;
 
 function setup() {
-    createCanvas( totalwidth, totalheight  );
+    createCanvas(totalwidth, totalheight);
     frameRate(100);
-    fill(200 ,alpha = 100) ; 
-    environ =  new Environment() ; 
+    fill(200, alpha = 100);
+    environ = new Environment();
+    environ.addForce(new createVector(0 , 1)) ; 
 
-    for(let i =0 ; i < 1 ; i++)
-        environ.addBall(new Ball(random(51 , 1000 ) , random( 51, 700), random(20 , 60) , environ)) ;
-    
-    environ.applyForce(new createVector(0 , 0.5)) ; 
-    environ.addForce(new createVector(0.3 , 0 )) ; 
+    for (let i = 0; i < 1; i++)
+        environ.addBall(new Ball(random(51, 1000), random(51, 700), random(20, 60), environ));
+
 }
+
 
 function draw() {
     background(255);
-    environ.updateAndShowAllBalls() ;
+    environ.update();
 }
 
 
 
-class Environment
-{
-    constructor(){
-        this.ballsArray = [] ; 
-        this.boundLeft = 50  ; 
-        this.boundRight = canvasWidth - 100 ; 
-        this.boundTop =  50 ;
-        this.boundBottom =  canvasHeight-100 ; 
+class Environment {
+
+
+    constructor() {
+        this.objectsArray = [];
+        this.forcesArray = [] ;
+        this.boundLeft = 50;
+        this.boundRight = canvasWidth - 100;
+        this.boundTop = 50;
+        this.boundBottom = canvasHeight - 100;
     }
 
-    update(){
-        rect(this.boundLeft , this.boundTop , this.boundRight , this.boundBottom) ; 
+
+    update() {
+        rect(this.boundLeft, this.boundTop, this.boundRight, this.boundBottom);
+        this.forcesArray.forEach(force=>{
+            this.applyForce(force) ; 
+        })
+        this.updateAndShowAllBalls();
     }
 
-    addBall(ball){
-        this.ballsArray.push(ball) ; 
+
+
+    addBall(ball) {
+        this.objectsArray.push(ball);
     }
 
-    updateAndShowAllBalls(){
-        this.update() ; 
-        this.ballsArray.forEach(ball=>ball.updateAndShow() ) ; 
+    updateAndShowAllBalls() {
+        this.objectsArray.forEach(ball => ball.updateAndShow());
     }
 
-    showAllBalls(){
-        this.ballsArray.forEach(ball=>ball.show()) ; 
+    showAllBalls() {
+        this.objectsArray.forEach(ball => ball.show());
     }
 
-    addForce(force){this.ballsArray.forEach(ball=>ball.addForce(force))}
-    applyForce(force){this.ballsArray.forEach(ball=>ball.applyForce(force))}
+    addForce(force) {
+        this.forcesArray.push(force) ; 
+    }
+    applyForce(force) {
+        this.objectsArray.forEach(ball => ball.applyForce(force))
+    }
 }
 
 
 
-class Ball{
+class Ball {
 
-    constructor(x , y,  radius  , environment){
-      this.environment = environment; 
-      this.radius = radius ;
-      this.loc = new createVector( x ,y) ;
-      this.velocityLimit = Number.POSITIVE_INFINITY ; 
-      this.vel = new createVector(0 , 0) ; 
-      this.acc = new createVector(0 , 0) ;
-      this.dampeningFactor = 0.90 ; 
+    constructor(x, y, radius, environment) {
+        this.environment = environment;
+        this.radius = radius;
+        this.loc = new createVector(x, y);
+        this.velocityLimit = Number.POSITIVE_INFINITY;
+        this.vel = new createVector(0, 0);
+        this.acc = new createVector(0, 0);
+        this.dampeningFactor = 0.90;
     }
 
-    updateAndShow(){
-        this.update() ; 
-        this.show() ; 
-    }
-    
-    update(){
-        console.log("Bob x , y = " , this.loc.x , this.loc.y , " Radius = " , this.radius) ; 
-       this.handleEnvironmentBound() ; 
-       this.loc.add(this.vel) ;
-       this.vel.add(this.acc) ;
-       this.vel.limit(this.velocityLimit) ; 
+    updateAndShow() {
+        this.update();
+        this.show();
     }
 
+    update() {
+        console.log("Bob x , y = ", this.loc.x, this.loc.y, " Radius = ", this.radius);
+        this.handleEnvironmentBound();
+        this.loc.add(this.vel);
+        this.vel.add(this.acc);
+        this.vel.limit(this.velocityLimit);
 
-    handleEnvironmentBound(){
-        if((this.loc.x-this.radius ) < this.environment.boundLeft){
-            this.vel.x = abs(this.vel.x) ;
-            this.vel.x*= this.dampeningFactor ; 
-        } 
+        this.acc.mult(0);
+    }
 
-        if(this.loc.x -10 > this.environment.boundRight){
-            this.vel.x = -1 * abs(this.vel.x) ; 
-            this.vel.x*= this.dampeningFactor ; 
-        } 
-        
-        if(this.loc.y - this.radius/2< this.environment.boundTop){
-            this.vel.y = abs(this.vel.y) ;   
-            this.vel.y*= this.dampeningFactor ; 
+
+    handleEnvironmentBound() {
+        if ((this.loc.x - this.radius) < this.environment.boundLeft) {
+            this.vel.x = abs(this.vel.x);
+            this.vel.x *= this.dampeningFactor;
         }
-    
-        if(this.loc.y-this.radius/2  > this.environment.boundBottom) 
-        {
-            this.vel.y=-1 * abs(this.vel.y)  ; 
-            this.vel.y*= this.dampeningFactor ; 
+
+        if (this.loc.x - 10 > this.environment.boundRight) {
+            this.vel.x = -1 * abs(this.vel.x);
+            this.vel.x *= this.dampeningFactor;
+        }
+
+        if (this.loc.y - this.radius / 2 < this.environment.boundTop) {
+            this.vel.y = abs(this.vel.y);
+            this.vel.y *= this.dampeningFactor;
+        }
+
+        if (this.loc.y - this.radius / 2 > this.environment.boundBottom) {
+            this.vel.y = -1 * abs(this.vel.y);
+            this.vel.y *= this.dampeningFactor;
         }
     }
-    
-    show()
-    {
-       ellipse(this.loc.x , this.loc.y  , this.radius *2  , this.radius * 2 ) ;
+
+    show() {
+        ellipse(this.loc.x, this.loc.y, this.radius * 2, this.radius * 2);
     }
 
-    applyForce(force){this.acc = force ; }
-    addForce(force){this.acc.add(force) ; }
-
-    setVelocityLimit(lim){this.velocityLimit = lim ; }
-    setAcceleration(acc) { this.acc = acc ;  }
-    setAcceleration(x , y ) { this.acc.x= x ;this.acc.y = y ;  }
+    applyForce(force) {
+        this.acc.add(force);
+    }
+    setVelocityLimit(lim) {
+        this.velocityLimit = lim;
+    }
+    setAcceleration(acc) {
+        this.acc = acc;
+    }
+    setAcceleration(x, y) {
+        this.acc.x = x;
+        this.acc.y = y;
+    }
 }
